166c2c926f7aa1b02725513eaa7693f4ccc07261
erika2852 <111336295+erika2852@users.noreply.github.com>
2023-11-17 18:37:48 +0900

    ㅁㅁ
    ㅁㅁ

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1700200126';
+const cacheName = 'chirpy-1700213366';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
